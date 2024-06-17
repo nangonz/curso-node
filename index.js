cdf1e73642db8f118010b0b5a29991085295de3f
nangonz <nangonz91@gmail.com>
2024-06-17 13:49:36 -0300

    setting up server with express

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-console.log('Hola mundo')
-const newVariable = '13456'
-console.log(newVariable)
+import express from 'express'
+import { PORT } from './config.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h3>Hellor World</h3>
+    `)
+})
+
+app.post('/login', (req, res) => {})
+
+app.post('/register', (req, res) => {})
+
+app.post('/logout', (req, res) => {})
+
+app.post('/protected', (req, res) => {})
+
+app.listen(PORT, () => {
+  console.log(`Server is now listening on port ${PORT}`)
+})
