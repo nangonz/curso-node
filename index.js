93937081d1e47afb2b777f9c4b05332ab4a6621d
nangonz <nangonz91@gmail.com>
2024-06-17 16:18:50 -0300

    add registration end-point

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { PORT } from './config.js'
+import { UserRepository } from './user-repository.js'
 
 const app = express()
 
@@ -11,7 +12,17 @@ app.get('/', (req, res) => {
 
 app.post('/login', (req, res) => {})
 
-app.post('/register', (req, res) => {})
+app.post('/register', (req, res) => {
+  const { username, password } = req.body
+  console.log(req.body)
+
+  try {
+    const id = UserRepository.create({ username, password })
+    res.send({ id })
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+})
 
 app.post('/logout', (req, res) => {})
 
