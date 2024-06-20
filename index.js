d8264e5d22b78d716fde116bc0e80b7232164c51
nangonz <nangonz91@gmail.com>
2024-06-19 23:15:36 -0300

    add middleware to get session token

--- index.js
+++ index.js
@@ -10,19 +10,22 @@ app.set('view engine', 'ejs')
 // MIDDLEWARES
 app.use(express.json())
 app.use(cookieParser())
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  let data = null
+  req.session = { user: null }
+
+  try {
+    data = jwt.verify(token, SECRET_JWT_KEY)
+    req.session.user = data
+  } catch {}
+  next() // seguir a la siguiente ruta o middleware
+})
 
 // END-POINTS
 app.get('/', (req, res) => {
-  const token = req.cookies.access_token
-  if (!token) return res.render('index')
-
-  try {
-    const data = jwt.verify(token, SECRET_JWT_KEY)
-    res.render('index', data) // { _id, username }
-  } catch (error) {
-    res.render('index')
-  }
-  res.render('index')
+  const { user } = req.session
+  res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
@@ -60,20 +63,18 @@ app.post('/register', async (req, res) => {
   }
 })
 
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res
+    .clearCookie('access_token')
+    .json({ message: 'Logout successful' })
+    .redirect('/')
+})
 
-app.post('/protected', (req, res) => {
-  const token = req.cookies.access_token
-  if (!token) {
-    return res.status(401).send('Access not authorized')
-  }
-
-  try {
-    const data = jwt.verify(token, SECRET_JWT_KEY)
-    res.render('protected', data) // { _id, username }
-  } catch (error) {
-    return res.status(401).send('access not authorized')
-  }
+app.get('/protected', (req, res) => {
+  const { user } = req.session
+  console.log(user)
+  if (!user) return res.status(403).send('Access not authorized')
+  res.render('protected', user)
 })
 
 app.listen(PORT, () => {
